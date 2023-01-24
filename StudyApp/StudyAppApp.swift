160cf64115622e23b521bf3bc4fb0e9864f68d46
Dominik <domkwi18@gmail.com>
2023-01-24 16:07:45 +0100

    Initial Commit

---
+++ StudyApp/StudyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyAppApp.swift
+//  StudyApp
+//
+//  Created by Dominik Kwiecien on 24/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct StudyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
