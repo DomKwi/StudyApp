829656ac40cee1003dd9e8c383b2f493459c3aed
Dominik <domkwi18@gmail.com>
2023-01-30 18:31:15 +0100

    building extensions

---
+++ StudyApp/Extensions/Timer.swift
@@ -0,0 +1,21 @@
+//
+//  Timer.swift
+//  StudyApp
+//
+//  Created by Dominik Kwiecien on 30/01/2023.
+//
+
+import Foundation
+
+extension SliderView {
+    final class ViewModel: ObservableObject {
+        @Published var isActive = false
+        @Published var showingAlert = false
+        @Published var time: String = "5:00"
+        @Published var minutes: Float = 5.0 {
+            didSet {
+                self.time = "\(Int(minutes)):00"
+            }
+        }
+    }
+}
