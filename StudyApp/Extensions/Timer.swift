0d418418a54f622e2695205fed823167b801c218
Dominik <domkwi18@gmail.com>
2023-01-30 18:51:56 +0100

    timer is done

--- StudyApp/Extensions/Timer.swift
+++ StudyApp/Extensions/Timer.swift
@@ -22,10 +22,12 @@ extension SliderView {
         private var endDate = Date()
         
         func startTimer(minutes: Float) -> () {
+            
             self.initTime = Int(minutes)
             self.endDate = Date()
             self.isActive = true
             self.endDate = Calendar.current.date(byAdding: .minute, value: Int(minutes), to: endDate)!
+            
         }
         
         func reset() {
@@ -34,6 +36,30 @@ extension SliderView {
             self.isActive = false
             self.time = "\(Int(minutes)):00"
             
+        }
+        
+        func countDown() {
+            
+            guard isActive else { return }
+            
+            let timeItIsNow = Date()
+            let difference = endDate.timeIntervalSince1970 - timeItIsNow.timeIntervalSince1970
+            
+            if difference <= 0 {
+                
+                self.isActive = false
+                self.time = "0:00"
+                self.showingAlert = true
+                return
+            }
+            
+            let date = Date(timeIntervalSince1970: difference)
+            let calendar = Calendar.current
+            let minutes = calendar.component(.minute, from: date)
+            let seconds = calendar.component(.second, from: date)
+            
+            self.minutes = Float(minutes)
+            self.time = String(format: "%d:%.2f", minutes, seconds)
         }
     }
 }
