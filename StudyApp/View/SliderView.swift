162afa7a39fff104fddccbe24c8d9a0ceec0f42b
Dominik <domkwi18@gmail.com>
2023-01-31 12:32:09 +0100

    adding timer to slider view

--- StudyApp/View/SliderView.swift
+++ StudyApp/View/SliderView.swift
@@ -9,16 +9,37 @@ import SwiftUI
 
 struct SliderView: View {
     @State var time = 15.0
+    @StateObject private var vm = ViewModel()
+    private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
+        
         ZStack {
             Color(hue: 0.58, saturation: 0.06, brightness: 1.0)
                 .edgesIgnoringSafeArea(.all)
             
+            
+            
             VStack{
                 SliderModelView(value: $time, in: 5...120)
                     .frame(width: 250, height: 250)
+                
+                HStack {
+                    Button("Start") {
+                        vm.minutes = Float(time)
+                        vm.startTimer(minutes: vm.minutes)
+                    }
+                    .disabled(vm.isActive)
+                    
+                    Text("\(vm.time)")
+                    
+                    Button("Reset", action: vm.reset)
+                        .tint(.red)
+                }
             }
+        }
+        .onReceive(timer) { _ in
+            vm.countDown()
         }
     }
 }
