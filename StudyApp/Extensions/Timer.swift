1e182077db721aa7db70fa3336e63b20facb8687
Dominik <domkwi18@gmail.com>
2023-01-31 13:21:25 +0100

    timer fixing

--- StudyApp/Extensions/Timer.swift
+++ StudyApp/Extensions/Timer.swift
@@ -11,8 +11,8 @@ extension SliderView {
     final class ViewModel: ObservableObject {
         @Published var isActive = false
         @Published var showingAlert = false
-        @Published var time: String = "5:00"
-        @Published var minutes: Float = 5.0 {
+        @Published var time: String = "15:00"
+        @Published var minutes: Float = 15.0 {
             didSet {
                 self.time = "\(Int(minutes)):00"
             }
