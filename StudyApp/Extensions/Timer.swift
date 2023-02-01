00a30b8fe4758a3449d8f162fdd75024e3116c93
Dominik <domkwi18@gmail.com>
2023-02-01 13:07:18 +0100

    creating a notification class

--- StudyApp/Extensions/Timer.swift
+++ StudyApp/Extensions/Timer.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UserNotifications
 
 extension SliderView {
     final class ViewModel: ObservableObject {
--- StudyApp/View/ContentView.swift
+++ StudyApp/View/ContentView.swift
@@ -8,21 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    @AppStorage("ALERT_KEY") var notificationAlert = true
+    //@AppStorage("ALERT_KEY") var notificationAlert = true
+    @State var notificationAlert = NotificationClass(notificationAlert1: true)
     
     var body: some View {
-        if notificationAlert == false {
+        if notificationAlert.notificationAlert1 == false {
             SliderView()
         } else {
             SliderView()
-                .alert(isPresented: $notificationAlert) {
+                .alert(isPresented: $notificationAlert.notificationAlert1) {
                     Alert(
                         title: Text("Notification"),
                         message: Text("Do you want to receive notifications from our app"),
                         primaryButton: .default(
                             Text("Accept"),
                             action: {
-                                notificationAlert = false
+                                notificationAlert.notificationAlert1 = false
                             }
                         ),
                         secondaryButton: .cancel()
