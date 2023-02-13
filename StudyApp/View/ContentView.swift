23759651fcf7fe0879df28b2775ab5b1f0211218
Dominik <domkwi18@gmail.com>
2023-02-13 16:28:26 +0100

    cleaning

--- StudyApp/View/ContentView.swift
+++ StudyApp/View/ContentView.swift
@@ -8,28 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    //@AppStorage("ALERT_KEY") var notificationAlert = true
-    @State var notificationAlert = NotificationClass(notificationAlert1: true)
     
     var body: some View {
-        if notificationAlert.notificationAlert1 == false {
             SliderView()
-        } else {
-            SliderView()
-                .alert(isPresented: $notificationAlert.notificationAlert1) {
-                    Alert(
-                        title: Text("Notification"),
-                        message: Text("Do you want to receive notifications from our app"),
-                        primaryButton: .default(
-                            Text("Accept"),
-                            action: {
-                                notificationAlert.notificationAlert1 = false
-                            }
-                        ),
-                        secondaryButton: .cancel()
-                    )
-                }
-        }
     }
 }
 
